d7763e49c8f25bd89673b071dcf067d1efe78ccb
Serhii Maksymchuk <smakdealcase@gmail.com>
2019-03-31 20:17:22 +0300

    Added GoodsController and one GET enpoint.

---
+++ MSFakesStudy/Controllers/GoodsController.cs
@@ -0,0 +1,28 @@
+﻿using MSFakesStudy.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MSFakesStudy.Controllers
+{
+    public class GoodsController : ApiController
+    {
+        private readonly IShoppingService _service;
+
+        public GoodsController()
+        {
+            _service = new ShoppingService();
+        }
+
+        [Route("api/goods/{name}")]
+        [HttpGet]
+        public IHttpActionResult GetByName(string name)
+        {
+            var result = _service.GetGoodsByName(name);
+            return this.Ok(result);
+        }
+    }
+}
